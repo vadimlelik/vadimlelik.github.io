560e87a7e4e2a3f6ae5d49ffceb2e0b4c1a0cfdc
vadimcvirko <promiti90@gmail.com>
2021-06-17 09:17:14 +0300

    ferst commit

---
+++ precache-manifest.3359c077e7e50c87bcfe77a2fdfe1cd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e929d4d446fbd1d0b19",
+    "url": "/static/css/main.e7505dc7.chunk.css"
+  },
+  {
+    "revision": "6e929d4d446fbd1d0b19",
+    "url": "/static/js/main.6e929d4d.chunk.js"
+  },
+  {
+    "revision": "3bcc369e09f53118cb6b",
+    "url": "/static/css/1.932e6e16.chunk.css"
+  },
+  {
+    "revision": "3bcc369e09f53118cb6b",
+    "url": "/static/js/1.3bcc369e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "23aefec5593db37247eaede8998dedc4",
+    "url": "/index.html"
+  }
+];
